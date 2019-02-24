c795d9089d61cb442eec2f8d715f867830ade751
Vika <kaledaviktoriya@gmail.com>
2019-02-25 01:42:49 +0300

    Add StoreItem, IStoreItemService, UpdateStoreItemRequest, UpdateStoreItemRequestValidator to models, StoreItemService with implementation of GetHiveSectionItemsAsync method and StoreItemManagementMappingProfile to services. Add StoreItemsController.

---
+++ KatlaSport.Services.Models/StoreItemManagement/UpdateStoreItemRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace KatlaSport.Services.StoreItemManagement
+{
+    /// <summary>
+    /// Represents a validator for <see cref="UpdateStoreItemRequest"/>
+    /// </summary>
+    public class UpdateStoreItemRequestValidator : AbstractValidator<UpdateStoreItemRequest>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdateStoreItemRequestValidator"/> class.
+        /// </summary>
+        public UpdateStoreItemRequestValidator()
+        {
+            RuleFor(r => r.ProductId).GreaterThan(0);
+            RuleFor(r => r.HiveSectionId).GreaterThan(0);
+            RuleFor(r => r.Quantity).GreaterThan(0);
+        }
+    }
+}
---
+++ KatlaSport.Services/StoreItemManagement/StoreItemManagementMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace KatlaSport.Services.StoreItemManagement
+{
+    using AutoMapper;
+    using DataAccessStoreItem = KatlaSport.DataAccess.ProductStore.StoreItem;
+
+    public class StoreItemManagementMappingProfile : Profile
+    {
+        public StoreItemManagementMappingProfile()
+        {
+            CreateMap<DataAccessStoreItem, StoreItem>();
+        }
+    }
+}
