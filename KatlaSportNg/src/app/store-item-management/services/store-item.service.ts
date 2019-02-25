d6a165a0b7c58fd0abde69724e774138abd79248
Vika <kaledaviktoriya@gmail.com>
2019-02-25 11:32:56 +0300

    Add StoreItem model, StoreItemListComponent and StoreItemService with implementation of getHiveSectionStoreItems method. Add markup for 'Hive section store items' page.

---
+++ KatlaSportNg/src/app/store-item-management/services/store-item.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
+import { Observable } from 'rxjs';
+import { StoreItem } from '../models/store-item';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class StoreItemService {
+    private url = environment.apiUrl + 'api/storeItems/';
+
+    constructor(private http: HttpClient) { }
+
+    getHiveSectionStoreItems(hiveSectionId: number): Observable<Array<StoreItem>> {
+        return this.http.get<Array<StoreItem>>(`${this.url}${hiveSectionId}`);
+    }
+}
